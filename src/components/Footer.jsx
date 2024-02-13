96fc50db113629e8f6027a765e38a04d9fb488dd
jugnugupta33 <jugnugupta33@gmail.com>
2024-02-13 19:42:21 +0530

    made the navbar sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='text-white bg-gray-700 text-lg text-center py-5 mt-8'>
+        <div className='text-white bg-gray-900 text-lg text-center py-5 mt-8'>
             Created By Jugnu Gupta &copy; 2024 FoodFusion
         </div>
     )
